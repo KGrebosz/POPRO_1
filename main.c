b67a2fdfe6a1396fbda9db4355639a2472279b35
Kacper Grębosz <01206283@pw.edu.pl>
2025-10-15 18:51:33 +0200

    Dodanie main.c z funkcją NWD

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int nwd(int a,int b)
+{
+    int pom;
+    while(b!=0)
+    {
+        pom=b;
+        b=a%b;
+        a=pom;
+    }
+    return a;
+}
+
+int main(void)
+{
+    int x,y;
+    scanf("%d %d", &x, &y);
+    int wynik=nwd(x,y);
+    printf("NWD, x i y, to = %d\n", wynik);
+}
